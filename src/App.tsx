a7da5d403d02a43b80f008045096b56d66deaea3
Antonio Andre <antonioandre1008@gmail.com>
2025-03-21 14:09:00 -0500

    :wrench: chore: instala e configura axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,23 @@
+import {useEffect} from "react";
+import api from "./api/api.ts";
+import {AxiosError} from "axios";
+import {ApiError} from "./types/apiTypes.ts";
+
 function App() {
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await api.get("/opening?id=1"); // Requisição GET autenticada
+                console.log(response);
+            } catch (error) {
+                const axiosError = error as AxiosError<ApiError>;
+                console.error("Erro ao buscar os dados: ", axiosError.response?.data.message, axiosError.response?.data.statusCode);
+            }
+        };
+
+        fetchData();
+    }, []);
 
     return (
         <>
---
+++ src/api/api.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL,
+});
+
+api.interceptors.request.use(
+    (config) => {
+        // const token = localStorage.getItem("token");
+        // if (token) {
+        //     config.headers.Authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFudG9uaW9hbmRyZTEwMDhAZ21haWwuY29tIiwiZXhwIjoxNzQyNjY3MTk1fQ.vqblUWB3lkGR2xh0plPoGR31lw_dzbJRfpXMwdMdJFQ`;
+        //
+        // }
+
+        config.headers.Authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFudG9uaW9hbmRyZTEwMDhAZ21haWwuY29tIiwiZXhwIjoxNzQyNjY3MTk1fQ.vqblUWB3lkGR2xh0plPoGR31lw_dzbJRfpXMwdMdJFQ`;
+
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+export default api;
