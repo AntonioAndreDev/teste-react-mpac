b555443608049d5b3dd80243703e34b7a83e35b4
Antonio Andre <antonioandre1008@gmail.com>
2025-03-22 01:47:32 -0500

    :art: style: ajusta responsividade na tela de LoginView.tsx

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -4,7 +4,7 @@ import {Toaster} from "@/components/ui/sonner.tsx";
 export default function AuthLayout() {
     return (
         <>
-            <div className="h-screen flex flex-col items-center justify-center mb-4">
+            <div className="h-screen flex flex-col items-center justify-center mb-4 p-4">
                 <img src="/assets/mpac-logo.png" alt="Logo do Ministério Público do Estado do Acre"/>
                 <Outlet/>
             </div>
